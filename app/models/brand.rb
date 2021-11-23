62b21fb2f91ca22409a90026238850229bdf0b3b
Emily Underhill <emilyru93@gmail.com>
2021-11-23 12:49:29 +0000

    validations and associations added

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,16 @@
 class Brand < ApplicationRecord
+  has_many :saved_brands
+
+  validates :name, presence: true, uniqueness: true
+  validates :website_url, presence: true
+  validates :price_category, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_earth, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_people, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_animals, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_materials, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :about, presence: true, length: { minimum: 100 }
+  validates :address, presence: true
+  validates :style, inclusion: { in: %w[modern outdoor minimalist retro boujie arty scandinavian grunge formal
+                                        lounge boho] }
+  validates :approved, presence: true
 end
