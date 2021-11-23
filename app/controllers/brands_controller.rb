db063ec6b063f2aa83b8b1f2d1b4ed7fbd3af2fe
Blanca Rivero <briveroanglada@gmail.com>
2021-11-23 17:57:16 +0000

    honestly so many changes but naughty action has been finalised and I tested it and it works hehehehe

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,10 +1,17 @@
 class BrandsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :naughty ]
 
   def naughty
   #also create a view - for its own page
-    @brand = brand.find(params[:id])
+    @brand = Brand.find(params[:id])
     @newsarticles = NewsArticle.all
+    @news = @newsarticles.order(created_at: :desc)
+    @topnews = @news.first(3)
+    @allbrands = Brand.all
+    @similarbrands = @allbrands.select do |newbrand|
+      newbrand.price_category = @brand.price_category && newbrand.style = @brand.style
+    end
+    # price and style overlap
   end
-
 
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,14 +3,13 @@ class Brand < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   validates :website_url, presence: true
-  validates :price_category, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_earth, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_people, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_animals, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_materials, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :price_category, numericality: { only_integer: true }, inclusion: { in: 1..5 }, allow_blank: true
+  validates :rating_earth, numericality: { only_integer: true }, inclusion: { in: 1..5 }, allow_blank: true
+  validates :rating_people, numericality: { only_integer: true }, inclusion: { in: 1..5 }, allow_blank: true
+  validates :rating_animals, numericality: { only_integer: true }, inclusion: { in: 1..5 }, allow_blank: true
+  validates :rating_materials, numericality: { only_integer: true }, inclusion: { in: 1..5 }, allow_blank: true
   validates :style, presence: true, inclusion: { in: %w[modern outdoor minimalist retro boujie arty scandinavian grunge
                                                         formal lounge boho] }
-  validates :approved, presence: true
 
   def average_rating
     total_rating = rating_animals + rating_earth + rating_materials + rating_people
