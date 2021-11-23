541635983ceb4dea06a149abf08f9220300682cb
Emily Underhill <emilyru93@gmail.com>
2021-11-23 14:47:48 +0000

    validations corrected and average_rating method added

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,16 +1,20 @@
 class Brand < ApplicationRecord
+
   has_many :saved_brands
 
   validates :name, presence: true, uniqueness: true
   validates :website_url, presence: true
-  validates :price_category, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_earth, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_people, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_animals, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :rating_materials, presence: true, numericality: { only_integer: true }, inclusion: { in: 1..5 }
-  validates :about, presence: true, length: { minimum: 100 }
-  validates :address, presence: true
+  validates :price_category, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_earth, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_people, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_animals, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+  validates :rating_materials, numericality: { only_integer: true }, inclusion: { in: 1..5 }
   validates :style, inclusion: { in: %w[modern outdoor minimalist retro boujie arty scandinavian grunge formal
                                         lounge boho] }
   validates :approved, presence: true
+
+  def average_rating
+    total_rating = rating_animals + rating_earth + rating_materials + rating_people
+    (total_rating / 4)
+  end
 end
