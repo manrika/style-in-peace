c13b3a5d96f30467614a7e52b84a2e6e71e40fd8
Flaminia von Grolman <flaminia@vgrolman.de>
2021-11-23 17:00:01 +0100

    added new/create

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,8 +1,26 @@
 class BrandsController < ApplicationController
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+    @brand.user = current_user
+    if @brand.save
+      redirect_to brand_path(@brand)
+    else
+      render :new
+    end
+  end
 
   def explore
     @brands = Brand.all
     @newsarticles = NewsArticle.all
   end
 
+  private
+
+  def brand_params
+    params.require(:brand).permit(:name, :webiste_url, :insta_url, :price_category, :rating_earth, :rating_people, :rating_animals,:rating_materials, :about, :why_we_love_them, :address, :splash_image, :style)
+  end
 end
