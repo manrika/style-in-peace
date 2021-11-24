43a6fa88c9f40e30acfc79499dafa482f2d2a587
Blanca Rivero <briveroanglada@gmail.com>
2021-11-24 15:12:48 +0000

    url and blurb validations changed

---
+++ app/models/news_article.rb
@@ -0,0 +1,7 @@
+class NewsArticle < ApplicationRecord
+  validates :title, presence: true
+  validates :url, presence: true, format: { with: /(ftp|http|https):\/\/[^ "]+/ }
+  validates :blurb, presence: true, length: { maximum: 200 }
+  validates :image, presence: true
+  validates :date, presence: true
+end
