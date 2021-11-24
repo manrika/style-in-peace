7408d46eb61112d4ceb9c86f00ef83118ccfadd1
Blanca Rivero <briveroanglada@gmail.com>
2021-11-24 15:09:41 +0000

    have changed the url and the blurb validations

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,8 +1,8 @@
 class NewsArticle < ApplicationRecord
 
-  validates :title, presence: true, format { with: /^(ftp|http|https):\/\/[^ "]+$/ }
-  validates :url, presence: true
-  validates :blurb, presence: true, length: { in: 100..200 }
-  validates :image, presence: true #set rule so it is always a specific img file once seed is gone
+  validates :title, presence: true
+  validates :url, presence: true, format: { with: /(ftp|http|https):\/\/[^ "]+/ }
+  validates :blurb, presence: true, length: { maximum: 200 }
+  validates :image, presence: true
   validates :date, presence: true
 end
