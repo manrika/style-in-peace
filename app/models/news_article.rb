4be6ba2b9b8802280ac9a7a2716ea7d52c4cac0d
Blanca Rivero <briveroanglada@gmail.com>
2021-11-24 11:42:38 +0000

    worked on the website_url validation

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,6 +1,6 @@
 class NewsArticle < ApplicationRecord
 
-  validates :title, presence: true
+  validates :title, presence: true, format { with: /^(ftp|http|https):\/\/[^ "]+$/ }
   validates :url, presence: true
   validates :blurb, presence: true, length: { in: 100..200 }
   validates :image, presence: true #set rule so it is always a specific img file once seed is gone
