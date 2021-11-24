8c5e215733819c95d3a6303a8c72fd2503f8f3bd
Blanca Rivero <briveroanglada@gmail.com>
2021-11-24 15:11:08 +0000

    testing

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,8 +1,8 @@
-class NewsArticle < ApplicationRecord
+# class NewsArticle < ApplicationRecord
 
-  validates :title, presence: true
-  validates :url, presence: true, format: { with: /(ftp|http|https):\/\/[^ "]+/ }
-  validates :blurb, presence: true, length: { maximum: 200 }
-  validates :image, presence: true
-  validates :date, presence: true
-end
+#   validates :title, presence: true
+#   validates :url, presence: true, format: { with: /(ftp|http|https):\/\/[^ "]+/ }
+#   validates :blurb, presence: true, length: { maximum: 200 }
+#   validates :image, presence: true
+#   validates :date, presence: true
+# end
