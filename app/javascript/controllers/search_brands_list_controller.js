97e78d0cc4160d604d27ece10241dc09834c1374
manrika malka <manrika.m@gmail.com>
2021-11-28 13:29:58 +0000

    search bar nearlyy there

--- app/javascript/controllers/search_brands_list_controller.js
+++ app/javascript/controllers/search_brands_list_controller.js
@@ -1,18 +1,35 @@
 import { Controller } from "stimulus";
 import { useClickOutside } from "stimulus-use";
 
-
 export default class extends Controller {
-  static targets = ['list'];
+  static targets = ['list', 'form', 'input'];
+
+  connect() {
+    console.log("Hiding"); // remove me
+    this.listTarget.classList.add('d-none');
+  }
 
   displayBrands() {
-    console.log("Hey you got this boo"); // remove me
+    console.log("Displaying"); // remove me
     this.listTarget.classList.remove('d-none');
-    useClickOutside(this)
+    useClickOutside(this);
   }
 
   clickOutside(event) {
-    event.preventDefault()
+    console.log("Hiding"); // remove me
+    event.preventDefault();
     this.listTarget.classList.add('d-none');
+  }
+
+  updateBrandsList(event) {
+    console.log("Updating"); // remove me
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+    .then(response => response.text())
+    .then((data) => {
+      this.listTarget.outerHTML = data;
+    })
+    // this.listTarget.classList.remove('d-none');
+    // event.preventDefault();
   }
 }
