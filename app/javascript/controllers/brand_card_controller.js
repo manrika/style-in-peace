132600913567281d9cf067afa501197e69857b0e
Emily Underhill <emilyru93@gmail.com>
2021-12-01 13:26:37 +0000

    shadow added

--- app/javascript/controllers/brand_card_controller.js
+++ app/javascript/controllers/brand_card_controller.js
@@ -2,16 +2,18 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
-  static targets = ["card", "info"]
+  static targets = ["shadow", "card", "info"]
 
   changeCard() {
     this.cardTarget.classList.add('hidden')
     this.infoTarget.classList.remove('hidden')
+    this.shadowTarget.classList.add('shadowed')
   }
 
   changeCardBack() {
     this.cardTarget.classList.remove('hidden')
     this.infoTarget.classList.add('hidden')
+    this.shadowTarget.classList.remove('shadowed')
   }
 
 }
